c24945f2c4309d44131ec9b8be609fac9cfbec37
salllman84 <salmanulfat73@gmail.com>
2024-12-07 08:19:21 +0500

    Resolved vulnerabilities and updated dependencies

---
+++ src/pages/Menu.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Menu = () => {
+    return (
+        <div>
+            <h1>Our Menu</h1>
+            <p>Check out our delicious menu items here.</p>
+        </div>
+    );
+};
+
+export default Menu;
